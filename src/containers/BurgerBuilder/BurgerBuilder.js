827b748e792cbdbc019ba442979f7644e27e3d29
Aishwarya Warkari <ashwarkari15@gmail.com>
2020-12-11 00:11:13 +0530

    final commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,174 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import axios from "../../axios-orders";
+
+import Aux from "../../hoc/Auxilary/Auxilary";
+import Modal from "../../components/UI/Modal/Modal";
+import Burger from "../../components/Burger/Burger";
+import Spinner from "../../components/UI/Spinner/Spinner";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import WithErrorHandler from "../../hoc/WithErrorHandler/WithErrorHandler";
+import * as actions from "../../store/actions/index";
+
+export class BurgerBuilder extends Component {
+  state = {
+    purchasing: false,
+  };
+
+  componentDidMount() {
+    this.props.onInitIngredients();
+  }
+
+  // addIngredientHandler = (type) => {
+  //     const oldCount = this.state.ingredients[type];
+  //     const updatedCount = oldCount + 1;
+
+  //     const updatedIngredients = {
+  //         ...this.state.ingredients,
+  //     };
+  //     updatedIngredients[type] = updatedCount;
+
+  //     const priceAddition = INGREDIENT_PRICES[type];
+  //     const oldPrice = this.state.totalPrice;
+  //     const newPrice = oldPrice + priceAddition;
+
+  //     this.setState({
+  //         ingredients: updatedIngredients,
+  //         totalPrice: newPrice,
+  //     });
+  //     this.updatePurchaseState(updatedIngredients);
+  // };
+
+  // removeIngredientHandler = (type) => {
+  //     const oldCount = this.state.ingredients[type];
+
+  //     if (oldCount <= 0) {
+  //         return;
+  //     }
+  //     const updatedCount = oldCount - 1;
+
+  //     const updatedIngredients = {
+  //         ...this.state.ingredients,
+  //     };
+  //     updatedIngredients[type] = updatedCount;
+
+  //     const priceDeduction = INGREDIENT_PRICES[type];
+  //     const oldPrice = this.state.totalPrice;
+  //     const newPrice = oldPrice - priceDeduction;
+
+  //     this.setState({
+  //         ingredients: updatedIngredients,
+  //         totalPrice: newPrice,
+  //     });
+  //     this.updatePurchaseState(updatedIngredients);
+  // };
+
+  updatePurchaseState = (ingredients) => {
+    const sum = Object.keys(ingredients)
+      .map((igKey) => ingredients[igKey])
+      .reduce((sum, el) => sum + el, 0);
+
+    return sum > 0;
+  };
+
+  purchaseHandler = () => {
+    if (this.props.isAuthenticated) {
+      this.setState({ purchasing: true });
+    } else {
+      this.props.onSetAuthRedirectPath("/checkout");
+      this.props.history.push("/auth");
+    }
+  };
+
+  purchaseCancelHandler = () => {
+    this.setState({ purchasing: false });
+  };
+
+  purchaseContinueHandler = () => {
+    this.props.onInitPurchase();
+    this.props.history.push("/checkout");
+  };
+
+  render() {
+    const disabledInfo = {
+      ...this.props.ings,
+    };
+
+    for (const key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
+    }
+
+    let orderSummary = null;
+    let burger = this.props.error ? (
+      <p>Ingredients can't be loaded</p>
+    ) : (
+      <Spinner />
+    );
+
+    if (this.props.ings) {
+      burger = (
+        <Aux>
+          <Burger ingredients={this.props.ings} />
+          <BuildControls
+            isAuth={this.props.isAuthenticated}
+            ingredientAdded={this.props.onIngredientAdded}
+            ingredientRemoved={this.props.onIngredientRemoved}
+            disabled={disabledInfo}
+            purchasable={this.updatePurchaseState(this.props.ings)}
+            ordered={this.purchaseHandler}
+            price={this.props.price}
+          />
+        </Aux>
+      );
+
+      orderSummary = (
+        <OrderSummary
+          price={this.props.price}
+          ingredients={this.props.ings}
+          purchaseCancelled={this.purchaseCancelHandler}
+          purchaseContinued={this.purchaseContinueHandler}
+        />
+      );
+    }
+
+    return (
+      <Aux>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          {orderSummary}
+        </Modal>
+        {burger}
+      </Aux>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    ings: state.burgerBuilder.ingredients,
+    price: state.burgerBuilder.totalPrice,
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
+    onIngredientRemoved: (ingName) =>
+      dispatch(actions.removeIngredient(ingName)),
+    onInitIngredients: () => dispatch(actions.initIngredients()),
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: (path) =>
+      dispatch(actions.setAuthRedirectPath(path)),
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WithErrorHandler(BurgerBuilder, axios));
---
+++ src/store/sagas/auth.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import { put, delay } from 'redux-saga/effects';
+
+import * as actions from '../actions/index';
+
+export function* logoutSaga(action) {
+    yield localStorage.removeItem('token');
+    yield localStorage.removeItem('userId');
+    yield localStorage.removeItem('expirationDate');
+    yield put(actions.logoutSucceed());
+}
+
+export function* checkAuthTimeoutSaga(action) {
+    yield delay(action.expirationTime * 1000);
+    yield put(actions.logout());
+}
+
+export function* authUserSaga(action) {
+    yield put(actions.authStart());
+
+    const authData = {
+        email: action.email,
+        password: action.password,
+        returnSecureToken: true,
+    };
+
+    let url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCEOEhd1CpekGDbD_9MgaPxISQ_5AgykLs';
+    if (!action.isSignUp) {
+        url =
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCEOEhd1CpekGDbD_9MgaPxISQ_5AgykLs';
+    }
+
+    try {
+        const response = yield axios.post(url, authData);
+        yield localStorage.setItem('token', response.data.idToken);
+        yield localStorage.setItem('userId', response.data.localId);
+        yield localStorage.setItem(
+            'expirationDate',
+            new Date(new Date().getTime() + response.data.expiresIn * 1000)
+        );
+        yield put(actions.authSuccess(response.data.idToken, response.data.localId));
+        yield put(actions.checkAuthTimeout(response.data.expiresIn));
+    } catch (err) {
+        yield put(actions.authFail(err.response.data.error));
+    }
+}
+
+export function* authCheckStateSaga(action) {
+    const token = yield localStorage.getItem('token');
+    if (!token) {
+        yield put(actions.logout());
+    } else {
+        let expirationDate = yield localStorage.getItem('expirationDate');
+        expirationDate = yield new Date(expirationDate);
+        if (expirationDate > new Date()) {
+            const userId = yield localStorage.getItem('userId');
+            yield put(actions.authSuccess(token, userId));
+            yield put(
+                actions.checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000)
+            );
+        } else {
+            yield put(actions.logout());
+        }
+    }
+}
